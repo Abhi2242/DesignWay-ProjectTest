486aca1e9b50471357ece28ccf020030d24971e2
SMARTGEEK <abhishek.vengurlekar@genora.co.uk>
2023-11-08 11:25:54 +0530

    code update

--- app/src/main/java/com/smartgeek/designwayproject/activities/dogpages/DogBreedActivity.kt
+++ app/src/main/java/com/smartgeek/designwayproject/activities/dogpages/DogBreedActivity.kt
@@ -1,5 +1,7 @@
 package com.smartgeek.designwayproject.activities.dogpages
 
+// This code written by SMARTGEEK
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
